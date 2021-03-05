b33a979d73af7c6a4b1b7956ed51a879900280b7
scott95100 <scott95100@gmail.com>
2021-03-04 20:37:32 -0700

    components/login and signup built out

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -43,6 +43,8 @@ const Signup = () => {
         }
     }
 
+    if (redirect) return <Redirect to="/login" />
+
     return (
         <div className="row mt-4">
             <div className="col-md-7 offset-md-3">
